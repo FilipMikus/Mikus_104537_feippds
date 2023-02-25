10277a2f0c7fa9e6d9a96eaaa686abcbc8e51f8e
Filip Mikus <xmikusf@stuba.sk>
2023-02-25 18:36:20 +0100

    feat: pridanie globálnych (zdieľaných) premenných
    Pridanie globálnych (zdieľaných) premenných v súbore 01_bakery.py.

--- 01_bakery.py
+++ 01_bakery.py
@@ -5,3 +5,7 @@ Modul obsahujúci implementáciu Bakery algorytmu.
 __author__ = "Filip Mikuš"
 __email__ = "xmikusf@stuba.sk"
 __license__ = "MIT"
+
+PROCES_POCET: int = 11
+tiket: list = [0 for i in range(PROCES_POCET)]
+vyber_tiket: list = [False for j in range(PROCES_POCET)]
