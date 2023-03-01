a5eda0fa5b5dc6ff6804effb8f0567c99aacce5c
Filip Mikus <xmikusf@stuba.sk>
2023-03-01 18:01:26 +0100

    feat: pridanie funkcie zakaznik_proces
    Pridanie funkcie zakaznik_proces simulujúcu proces zákazníka v súbore 02_barbershop.py.

--- 02_barbershop.py
+++ 02_barbershop.py
@@ -62,3 +62,48 @@ def rast_vlasov(proces_id: int):
 
     print(f"Zákazníkovi [id: {proces_id}] rastú vlasy.")
     sleep(5)
+
+
+def zakaznik_proces(proces_id: int, barbershop: Barbershop):
+    """
+    Funkcia simulujúca proces reprezentujúci zákazníka.
+
+    Argumenty:
+        proces_id: Identifikátor procesu.
+        barbershop: Barbershop objekt obsahujúci zdieľané premenné.
+    """
+
+    while True:
+        # zamknutie mutex-u pred manipuláciou s premennou čakáreň
+        barbershop.mutex.lock()
+        cakaren_prichod(proces_id)
+        # overenie, či je v čakárni voľné miesto
+        if barbershop.cakaren == CAKAREN_MAX_KAPACITA:
+            # plná čakáreň
+            # odomknutie mutex-u po manipulácii s premennou čakáreň
+            barbershop.mutex.unlock()
+            plna_cakaren_odchod(proces_id)
+        else :
+            # voľná čakáreň
+            barbershop.cakaren += 1
+            # odomknutie mutex-u po manipulácii s premennou čakáreň
+            barbershop.mutex.unlock()
+
+            # signalizácia (rendezvous 1), že zákazník je pripravený na ostrihanie
+            barbershop.zakaznik_pripraveny.signal()
+            # čakanie (rendezvous 1) na barbéra pripraveného na strihanie
+            barbershop.barber_pripraveny.wait()
+            strihanie_vlasov_zakaznika(proces_id)
+            # signalizácia (rendezvous 2), že zákazník je ostrihaný
+            barbershop.zakaznik_koniec_obsluhy.signal()
+            # čakanie (rendezvous 2) na dokončenie strihu barbérom
+            barbershop.barber_koniec_obsluhy.wait()
+
+            # zamknutie mutex-u pred manipuláciou s premennou čakáreň
+            barbershop.mutex.lock()
+            cakaren_odchod(proces_id)
+            barbershop.cakaren -= 1
+            # odomknutie mutex-u po manipulácii s premennou čakáreň
+            barbershop.mutex.unlock()
+
+            rast_vlasov(proces_id)
