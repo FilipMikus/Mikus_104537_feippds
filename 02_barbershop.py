fd9cd239478e502ee95298398b393b23d93f5cd2
Filip Mikus <xmikusf@stuba.sk>
2023-03-01 17:49:53 +0100

    feat: pridanie zdieľaného objektu Barbershop
    Pridanie zdieľaného objektu Barbershop v súbore 02_barbershop.py.

--- 02_barbershop.py
+++ 02_barbershop.py
@@ -8,5 +8,22 @@ __email__ = "xmikusf@stuba.sk, mariansebena@stuba.sk, xvavro@stuba.sk"
 __license__ = "MIT"
 
 
+from fei.ppds import Mutex, Semaphore
+
+
 ZAKAZNIK_POCET: int = 5
 CAKAREN_MAX_KAPACITA: int = 3
+
+
+class Barbershop(object):
+
+    def __init__(self):
+
+        self.mutex: Mutex = Mutex()
+        self.cakaren: int = 0
+        # rendezvous 1
+        self.zakaznik_pripraveny: Semaphore = Semaphore(0)
+        self.barber_pripraveny: Semaphore = Semaphore(0)
+        # rendezvous 2
+        self.zakaznik_koniec_obsluhy: Semaphore = Semaphore(0)
+        self.barber_koniec_obsluhy: Semaphore = Semaphore(0)
