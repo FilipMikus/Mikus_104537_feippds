220a6b4f3e0b4da6be25b6ce1af6200d528ecf30
Filip Mikus <xmikusf@stuba.sk>
2023-03-01 17:57:29 +0100

    feat: pridanie funkcii aktivit
    Pridanie vypisovacích funkcii jednotlivých aktivít v súbore 02_barbershop.py.

--- 02_barbershop.py
+++ 02_barbershop.py
@@ -8,7 +8,8 @@ __email__ = "xmikusf@stuba.sk, mariansebena@stuba.sk, xvavro@stuba.sk"
 __license__ = "MIT"
 
 
-from fei.ppds import Mutex, Semaphore
+from fei.ppds import Mutex, Semaphore, print
+from time import sleep
 
 
 ZAKAZNIK_POCET: int = 5
@@ -27,3 +28,37 @@ class Barbershop(object):
         # rendezvous 2
         self.zakaznik_koniec_obsluhy: Semaphore = Semaphore(0)
         self.barber_koniec_obsluhy: Semaphore = Semaphore(0)
+
+
+def strihanie_vlasov_zakaznika(proces_id: int):
+
+    print(f"Zákazník [id: {proces_id}] využíva služby barbéra.")
+    sleep(3)
+
+
+def strihanie_vlasov_barberom(proces_id: int):
+
+    print(f"Barbér [id: {proces_id}] poskytuje svoje služby.")
+    sleep(3)
+
+
+def cakaren_prichod(proces_id: int):
+
+    print(f"Zákazník [id: {proces_id}] vchádza do barbershopu.")
+
+
+def cakaren_odchod(proces_id: int):
+
+    print(f"Zákazník [id: {proces_id}] odchádza z barbershopu.")
+
+
+def plna_cakaren_odchod(proces_id: int):
+
+    print(f"Čakáreň je plná, zákazník [id: {proces_id}] odchádza z barbershopu.")
+    sleep(5)
+
+
+def rast_vlasov(proces_id: int):
+
+    print(f"Zákazníkovi [id: {proces_id}] rastú vlasy.")
+    sleep(5)
