90a61d8c6b88140315ee3d8c0b6c7a176d200f9a
Filip Mikus <xmikusf@stuba.sk>
2023-03-01 17:41:18 +0100

    docs: pridanie dokumentácie modulu 02_barbershop
    Pridanie dokumentácie (docstring) modulu 02_barbershop v súbore 02_barbershop.py.

---
+++ 02_barbershop.py
@@ -0,0 +1,8 @@
+"""
+Modul obsahujúci implementáciu holičstva s predbiehaním.
+"""
+
+
+__authors__ = "Filip Mikuš, Marián Šebeňa"
+__email__ = "xmikusf@stuba.sk, mariansebena@stuba.sk, xvavro@stuba.sk"
+__license__ = "MIT"
