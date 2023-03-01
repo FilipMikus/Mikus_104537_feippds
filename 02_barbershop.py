366707b4fbca2d4ad0d610daa9c93d4fff84fcee
Filip Mikus <xmikusf@stuba.sk>
2023-03-01 17:46:34 +0100

    feat: pridanie globálnych premenných
    Pridanie globálnych premenných v súbore 02_barbershop.py.

--- 02_barbershop.py
+++ 02_barbershop.py
@@ -6,3 +6,7 @@ Modul obsahujúci implementáciu holičstva s predbiehaním.
 __authors__ = "Filip Mikuš, Marián Šebeňa"
 __email__ = "xmikusf@stuba.sk, mariansebena@stuba.sk, xvavro@stuba.sk"
 __license__ = "MIT"
+
+
+ZAKAZNIK_POCET: int = 5
+CAKAREN_MAX_KAPACITA: int = 3
