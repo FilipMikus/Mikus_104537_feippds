aa941adc74cb3c561695e1c1d8b36ccd9fbf4bdc
Filip Mikus <xmikusf@stuba.sk>
2023-03-11 20:18:19 +0100

    feat: pridanie funkcii aktivit
    Pridanie vypisovacích funkcii jednotlivých aktivít v súbore 03_philosophers.py.

--- 03_philosophers.py
+++ 03_philosophers.py
@@ -20,3 +20,15 @@ class Jedalen:
     def __init__(self):
 
         self.vidlicky: list[Mutex] = [Mutex() for _ in range(FILOZOF_POCET)]
+
+
+def premyslanie(proces_id: int):
+
+    print(f"Filozof [id: {proces_id}] premýšla.")
+    sleep(3)
+
+
+def stravovanie(proces_id: int):
+
+    print(f"Filozof [id: {proces_id}] sa stravuje.")
+    sleep(3)
