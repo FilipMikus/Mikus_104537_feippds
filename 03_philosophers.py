6027a1d2a5a893b480272370387d947404aa58c3
Filip Mikus <xmikusf@stuba.sk>
2023-03-11 20:11:58 +0100

    docs: pridanie dokumentácie modulu 03_philosophers
    Pridanie dokumentácie (docstring) modulu 03_philosophers v súbore 03_philosophers.py.

---
+++ 03_philosophers.py
@@ -0,0 +1,8 @@
+"""
+Modul obsahujúci implementáciu večerajúcich filozofov (ľavák-pravák).
+"""
+
+
+__authors__ = "Filip Mikuš, Tomáš Vavro"
+__email__ = "xmikusf@stuba.sk, mariansebena@stuba.sk, xvavro@stuba.sk"
+__license__ = "MIT"
