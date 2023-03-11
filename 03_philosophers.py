ba80db4915c7897e684fb25b7dd51dbf76e3cc6b
Filip Mikus <xmikusf@stuba.sk>
2023-03-11 20:23:18 +0100

    docs: pridanie dokumentacie funkcii a metody
    Pridanie dokumentácie (docstring) funkcii a metódy v súbore 03_philosophers.py.

--- 03_philosophers.py
+++ 03_philosophers.py
@@ -16,25 +16,53 @@ FILOZOF_POCET: int = 5
 
 
 class Jedalen:
+    """
+    Trieda reprezentujúca Jedalen a jeho zdieľané premenné.
+
+    Atribúty:
+        vidlicky: List Mutex-ov reprezentujúce vidličky.
+    """
 
     def __init__(self):
+        """
+        Metóda na inicializáciu objektu Jedalen a jeho zdieľaných premenných.
+        """
 
         self.vidlicky: list[Mutex] = [Mutex() for _ in range(FILOZOF_POCET)]
 
 
 def premyslanie(proces_id: int):
+    """
+    Funkcia reprezentujúca premýšlanie filozofa.
+
+    Argumenty:
+        proces_id: Identifikátor procesu.
+    """
 
     print(f"Filozof [id: {proces_id}] premýšla.")
     sleep(3)
 
 
 def stravovanie(proces_id: int):
+    """
+    Funkcia reprezentujúca stravovanie filozofa.
+
+    Argumenty:
+        proces_id: Identifikátor procesu.
+    """
 
     print(f"Filozof [id: {proces_id}] sa stravuje.")
     sleep(3)
 
 
 def filozof_proces(proces_id: int, jedalen: Jedalen):
+    """
+    Funkcia simulujúca proces reprezentujúci filozofa.
+
+    Argumenty:
+        proces_id: Identifikátor procesu.
+        jedalen: Jedalen objekt obsahujúci zdieľané premenné.
+    """
 
     while True:
         premyslanie(proces_id)
@@ -55,6 +83,9 @@ def filozof_proces(proces_id: int, jedalen: Jedalen):
 
 
 def main():
+    """
+    Funkcia vykonávajúca všeobecnú funkcionalitu.
+    """
 
     jedalen: Jedalen = Jedalen()
     filozofi: list[Thread] = []
