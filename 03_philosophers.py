b3d200a6c97f2886eea82e3b2bc4562355f04913
Filip Mikus <xmikusf@stuba.sk>
2023-03-11 20:13:37 +0100

    feat: pridanie globálnych premenných
    Pridanie globálnych premenných v súbore 03_philosophers.py.

--- 03_philosophers.py
+++ 03_philosophers.py
@@ -6,3 +6,10 @@ Modul obsahujúci implementáciu večerajúcich filozofov (ľavák-pravák).
 __authors__ = "Filip Mikuš, Tomáš Vavro"
 __email__ = "xmikusf@stuba.sk, mariansebena@stuba.sk, xvavro@stuba.sk"
 __license__ = "MIT"
+
+
+from fei.ppds import Mutex, Thread, print
+from time import sleep
+
+
+FILOZOF_POCET: int = 5
