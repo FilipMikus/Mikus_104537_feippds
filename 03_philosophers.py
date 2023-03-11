ce5702f8d28c5960c507cf147122d3ac95b87f91
Filip Mikus <xmikusf@stuba.sk>
2023-03-11 20:20:20 +0100

    feat: pridanie funkcie filozof_proces
    Pridanie funkcie filozof_proces simulujúcu proces filozofa v súbore 03_philosophers.py.

--- 03_philosophers.py
+++ 03_philosophers.py
@@ -32,3 +32,23 @@ def stravovanie(proces_id: int):
 
     print(f"Filozof [id: {proces_id}] sa stravuje.")
     sleep(3)
+
+
+def filozof_proces(proces_id: int, jedalen: Jedalen):
+
+    while True:
+        premyslanie(proces_id)
+
+        if proces_id == 0:
+            # filozof - ľavák
+            jedalen.vidlicky[(proces_id + 1) % FILOZOF_POCET].lock()
+            jedalen.vidlicky[proces_id].lock()
+        else:
+            # filozof - pravák
+            jedalen.vidlicky[proces_id].lock()
+            jedalen.vidlicky[(proces_id + 1) % FILOZOF_POCET].lock()
+
+        stravovanie(proces_id)
+
+        jedalen.vidlicky[(proces_id + 1) % FILOZOF_POCET].unlock()
+        jedalen.vidlicky[proces_id].unlock()
