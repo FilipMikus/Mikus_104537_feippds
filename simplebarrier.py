1d8b54b852bfd3596d359780ba724d4966302696
Filip Mikus <xmikusf@stuba.sk>
2023-03-20 10:18:44 +0100

    docs: pridanie dokumentácie modulu synchronizačného mechanizmu SimpleBarrier
    Pridanie dokumentácie modulu synchronizačného mechanizmu SimpleBarrier v súbore simplebarrier.py.

--- simplebarrier.py
+++ simplebarrier.py
@@ -1,9 +1,35 @@
+"""
+Modul obsahujúci implementáciu jednoducej bariéry s využitím objektu Event (fei.ppds).
+Zdroj: https://www.youtube.com/watch?v=3KJv9hFTx6k&t=876s
+"""
+
+
+__authors__ = "Filip Mikuš, Matúš Jókay"
+__email__ = "xmikusf@stuba.sk, matus.jokay@stuba.sk"
+__license__ = "MIT"
+
+
 from fei.ppds import Mutex, Event
 
 
 class SimpleBarrier:
+    """
+    Trieda implementujúca funkcionalitu synchronizačného mechanizmu bariéra.
+
+    Atribúty:
+        n: Premenná reprezentujúca celkový počet synchronizovaných procesov.
+        c: Premenná reprezentujúca počítadlo procesov čakajúcich pri bariére.
+        mutex: Mutex na manipuláciu s premennou c.
+        event: Synchronizačný mechanizmus (Event).
+    """
 
     def __init__(self, n):
+        """
+        Metóda na inicializáciu objektu SimpleBarrier, jeho premenných a objektov.
+
+        Argumenty:
+            n: Počet synchronizovaných procesov.
+        """
 
         self.n = n
         self.c = 0
@@ -12,6 +38,9 @@ class SimpleBarrier:
 
 
     def wait(self):
+        """
+        Metóda implementujúca čakanie volajúceho procesu pri bariére.
+        """
 
         self.mutex.lock()
         if self.c == 0:
