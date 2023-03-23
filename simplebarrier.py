956bd2a5bafd0efe154ca57df29734c40e8717df
Filip Mikus <xmikusf@stuba.sk>
2023-03-24 00:36:19 +0100

    feat: úprava implementácie synchronizačného mechanizmu SimpleBarrier
    Úprava implementácie synchronizačného mechanizmu SimpleBarrier v súbore simplebarrier.py.

--- simplebarrier.py
+++ simplebarrier.py
@@ -1,5 +1,5 @@
 """
-Modul obsahujúci implementáciu jednoducej bariéry s využitím objektu Event (fei.ppds).
+Modul obsahujúci implementáciu jednoducej bariéry.
 Zdroj: https://www.youtube.com/watch?v=3KJv9hFTx6k&t=876s
 """
 
@@ -9,7 +9,7 @@ __email__ = "xmikusf@stuba.sk, matus.jokay@stuba.sk"
 __license__ = "MIT"
 
 
-from fei.ppds import Mutex, Event
+from fei.ppds import Mutex, Semaphore, print
 
 
 class SimpleBarrier:
@@ -20,7 +20,7 @@ class SimpleBarrier:
         n: Premenná reprezentujúca celkový počet synchronizovaných procesov.
         c: Premenná reprezentujúca počítadlo procesov čakajúcich pri bariére.
         mutex: Mutex na manipuláciu s premennou c.
-        event: Synchronizačný mechanizmus (Event).
+        semaphore: Synchronizačný mechanizmus (Semaphore).
     """
 
     def __init__(self, n):
@@ -34,7 +34,7 @@ class SimpleBarrier:
         self.n = n
         self.c = 0
         self.mutex = Mutex()
-        self.event = Event()
+        self.semaphore = Semaphore(0)
 
 
     def wait(self):
@@ -43,10 +43,25 @@ class SimpleBarrier:
         """
 
         self.mutex.lock()
-        if self.c == 0:
-            self.event.clear()
         self.c += 1
         if self.c == self.n:
-            self.event.signal()
+            self.c = 0
+            self.semaphore.signal(self.n)
         self.mutex.unlock()
-        self.event.wait()
+        self.semaphore.wait()
+
+
+    def wait_vypis(self, vlakno_vypis, vlakna_stretnutie_vypis):
+        """
+        Metóda implementujúca čakanie volajúceho procesu pri bariére.
+        """
+
+        self.mutex.lock()
+        self.c += 1
+        print(vlakno_vypis)
+        if self.c == self.n:
+            self.c = 0
+            print(vlakna_stretnutie_vypis)
+            self.semaphore.signal(self.n)
+        self.mutex.unlock()
+        self.semaphore.wait()
